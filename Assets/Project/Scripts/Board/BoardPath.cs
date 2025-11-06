0e6c1f77ac5017b4b9722a9d0da22018bc74098b
Junki Morimoto <25nm777x@vc.ibaraki.ac.jp>
2025-11-06 18:26:25 +0900

    gridの数値に対応するイベントの修正

--- Assets/Project/Scripts/Board/BoardPath.cs
+++ Assets/Project/Scripts/Board/BoardPath.cs
@@ -7,28 +7,13 @@ public class BoardPath : ScriptableObject
     [Tooltip("タイル間隔（XYの距離）")]
     public float tileSpacing = 5f;
 
-    [Tooltip("0=空白, 1=通常, 2=スタート, 3=ゴール, 4=イベントなど")]
+    [Tooltip("0=空白, 1=スタート, 2=ゴール, 3=右, 4=左, 5=上, 6=下")]
+    //3桁目は進行方向 下2桁はイベントマス　10=前進　11=後退　12=1回休み　13=もう一度振る　14=スタートに戻る
     public int[,] grid = new int[,] {
-        {2,10,11,4,1,1,1},   // 2=スタート
-        {1,1,1,10,14,12,11},
-        {1,1,1,1,1,13,1},
-        {1,1,14,13,1,3,0},   // 3=ゴール
+        {301,310,300,311,300,600,0},   // 1=スタート
+        {0,0,0,0,0,612,0},
+        {0,600,400,400,400,413,0},   
+        {0,600,0,0,0,0,0},
+        {0,300,314,313,300,302,0},   // 2=ゴール
     };
-
-    /*[Tooltip("各マスのイベント情報（必要なら拡張）")]
-    public List<TileEvent> events = new List<TileEvent>()
-    {
-        new TileEvent(TileEvent.EventType.None),
-        new TileEvent(TileEvent.EventType.Forward, 3),
-        new TileEvent(TileEvent.EventType.None),
-        /*new TileEvent(TileEvent.EventType.SkipNext),
-        new TileEvent(TileEvent.EventType.Back, 1),
-        new TileEvent(TileEvent.EventType.None),
-        new TileEvent(TileEvent.EventType.None),
-        new TileEvent(TileEvent.EventType.Forward, 3),
-        new TileEvent(TileEvent.EventType.None),
-        new TileEvent(TileEvent.EventType.ExtraTurn),
-        new TileEvent(TileEvent.EventType.None),
-        new TileEvent(TileEvent.EventType.GoToStart),
-    };*/
 }
